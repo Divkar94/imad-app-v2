43c9472c6ce7eaf1c5629a7b1d02242466495313
Divkar94 <dk587@snu.edu.in>
2017-04-19 02:10:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',function(req,res) { res.send('Article one will be served here')});
+app.get('/article-1',function(req,res) { res.send('Article 1 will be served here')});
+app.get('/article-2',function(req,res) { res.send('Article 2 will be served here')});
+app.get('/article-3',function(req,res) { res.send('Article 3 will be served here')});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
