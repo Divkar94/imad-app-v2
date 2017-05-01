fc3c7116ac19a0227a9cf773d2107ef8c9686df0
Divkar94 <dk587@snu.edu.in>
2017-05-01 17:42:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,16 @@
 // Counter part
 var button=document.getElementById('counter');
-var c=0;
 button.onclick = function(){
-    c=c+1;
-    var count = document.getElementById('count');
-    count.innerHTML=c.toString();
+    var request =new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            if(request.status ===200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+    }
+    request.open('GET','http://divkar94.imad.hasura-app.io/counter',true);
+    request.send(null);
 }
