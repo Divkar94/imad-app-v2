bf8486a0e557683e001fe33d29ae851920505193
Divkar94 <dk587@snu.edu.in>
2017-05-01 17:31:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,8 @@
 // Counter part
-var counter=document.getElementById('counter');
+var button=document.getElementById('counter');
+var c=0;
+button.onclick = function(){
+    c=c+1;
+    var count = document.getElementById('count');
+    count.innerHTML=c.toString();
+}
