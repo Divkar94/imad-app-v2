c58f3b992874d9d1dfbd623d83d6333594e9d341
Divkar94 <dk587@snu.edu.in>
2017-05-01 22:06:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,8 +22,8 @@ submit.onclick=function(){
   request.onreadystatechange=function(){
   if(request.readystate===XMLHttpRequest.DONE){
       if(request.status===200){
-         var names=['name1','name2','name3'];
-  var list='';
+      var names= JSON.parse(request.responseText);
+      var list='';
   for (var i=0;i<names.length;i++)
   {
       list+='<li>'+names[i]+'</li>';
