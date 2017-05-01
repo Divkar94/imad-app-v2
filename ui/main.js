f37c4762dd87676b2e79bfd62b16deae162fc6fe
Divkar94 <dk587@snu.edu.in>
2017-05-01 17:27:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,2 @@
-console.log('Loaded!');
-alert('Yo!');
-//var element = document.getElementById('main-text');
-//element.innerHTML = 'New Value';
-var img = document.getElementById('madi');
-var marginleft=0;
-function moveRight(){
-    marginleft = marginleft + 10;
-    img.style.marginLeft = marginleft +'px';
-}
-img.onclick = function() {
-     var interval = setInterval(moveRight,50);  
-};
+// Counter part
+var counter=document.getElementById('counter');
